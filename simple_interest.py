9335012384a3d4ce2e53c7d850f17f6f94b49032
Champ-Mark <chetanmathpal@gmail.com>
2019-03-31 16:45:22 +0530

    adding files

---
+++ simple_interest.py
@@ -0,0 +1,5 @@
+principle = int(input("Enter the principle amount :"))
+rate = float(input("Enter the Rate of interest :"))
+time = int(input("Enter the time in years  :"))
+si = principle*rate*time/100
+print(si)
